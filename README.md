b3b3df73d3bccea9b9df556aa8e23300d1a438e4
EmilianoG-byte <57567043+EmilianoG-byte@users.noreply.github.com>
2024-07-30 22:03:41 -0400

    add setup.py for installation

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 # MLNISE
 
-MLNISE is a python package with the implementation of the algorithms presented in the paper "Spectral Densities, Structured Noise and Ensemble Averaging within Open
+`mlnise` is a python package with the implementation of the algorithms presented in the paper "Spectral Densities, Structured Noise and Ensemble Averaging within Open
 Quantum Dynamics" (add a link here).
 
 
 ## Installation
-
-Installing MLNISE is straighforward using the package manager [pip](https://pip.pypa.io/en/stable/).
+We recommend creating a virtual environment using either `venv` or `miniconda`. Once the environment is active, installing `mlnise` is straightforward using the package manager [pip](https://pip.pypa.io/en/stable/).
 
 ```bash
 pip install .
@@ -18,6 +17,12 @@ pip install .
 import mlnise
 
 # include code here
+```
+
+or run the sample python script
+
+```bash
+python run_mlnise.py
 ```
 
 
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="mlnise",
+    version="0.1.0",
+    author="Yannick Holkamp, Emiliano Godinez, Ulrich Kleinekathöfer",
+    author_email="yholtkamp@constructor.university",
+    description="MLNISE is machine learning based implementation of the NISE algorithm for non markovian dynamics. ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ausstein/MLNISE",  # Replace with your project's URL
+    packages=find_packages(),  # Automatically find packages in the project
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
+    install_requires=[
+        "matplotlib~=3.9",
+        "numpy==1.26.4",
+        "scipy==1.14.0",
+        "torch==2.2.2",
+        "torchvision==0.17.2",
+        "tqdm==4.66.4",
+    ],
+)
