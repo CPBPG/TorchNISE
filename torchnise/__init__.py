7ffa132c0ece61adf0ab52bf19bb06024053e9af
Ausstein <yannick.holtkamp@gmail.com>
2024-09-04 18:23:12 +0200

    edited init for new file names

--- torchnise/__init__.py
+++ torchnise/__init__.py
@@ -1,2 +1,2 @@
-from . import example_spectral_functions, fft_noise_gen, pytorch_utility, units, nise,absorption
+from . import example_spectral_functions, fft_noise_gen, pytorch_utility, units, nise,absorption, spectral_density_generation,averaging_and_lifetimes
 #pytorch_utility.clean_temp_files()
