pylint absorption, averaging and lifetimes and fixed t_correction

torchnise/absorption.py
@@ -1,80 +1,106 @@
+"""
+This module provides functions to compute time domain absorption and convert it 
+to an absorption spectrum using FFT. It allows for optional damping to simulate 
+signal decay over time.
+"""
+
 import numpy as np
 from torchnise.pytorch_utility import smooth_damp_to_zero
-import torchnise.units as units
+from torchnise import units
 
 
-def absorption_time_domain(U,mu,use_damping=False,lifetime=1000,dt=1):
+
+def absorption_time_domain(time_evolution_operator, dipole_moments,
+                           use_damping=False, lifetime=1000, dt=1):
     """
     Calculate the time domain absorption based on the time evolution operator.
     
     Args:
-        U (numpy.ndarray): Time evolution operator with dimensions (realizations, timesteps, n_sites, n_sites).
-        mu (numpy.ndarray): Dipole moments with either shape (realizations, timesteps, n_sites, 3) for time-dependent cases,
-                            or shape (n_sites, 3) for time-independent cases.
-        use_damping (bool, optional): Whether to apply exponential damping to account for the lifetime of the state. Default is False.
-        lifetime (float, optional): lifetime for the damping. Default is 1000. Units are not important as long as dt and lifetime have
-                                    the same unit.
-        dt (float, optional): Time step size. Default is 1.
+        time_evolution_operator (numpy.ndarray): Time evolution operator with 
+            dimensions (realizations, timesteps, n_sites, n_sites).
+        dipole_moments (numpy.ndarray): Dipole moments with either shape 
+            (realizations, timesteps, n_sites, 3) for time-dependent cases, or 
+            shape (n_sites, 3) for time-independent cases.
+        use_damping (bool, optional): Whether to apply exponential damping to 
+            account for the lifetime of the state. Default is False.
+        lifetime (float, optional): Lifetime for the damping. Default is 1000. 
+            Units are not important as long as dt and lifetime have the same 
+            unit. dt (float, optional): Time step size. Default is 1.
     
     Returns:
         numpy.ndarray: Time domain absorption.
     
     Notes:
-        This function calculates the time domain absorption by summing over the contributions of different realizations,
-        timesteps, and sites. An optional exponential damping factor can be applied to simulate the decay of the signal
-        over time.
+        This function calculates the time domain absorption by summing over the 
+        contributions of different realizations, timesteps, and sites. An 
+        optional exponential damping factor can be applied to simulate the 
+        decay of the signal over time.
     """
-    n_sites=U.shape[-1]
-    realizations=U.shape[0]
-    timesteps=U.shape[1]
+    n_sites = time_evolution_operator.shape[-1]
+    realizations = time_evolution_operator.shape[0]
+    timesteps = time_evolution_operator.shape[1]
 
-    if len(mu.shape)==2: ## time dependence is not supplied
-        mu=np.tile(mu,(realizations,timesteps,1,1)) ##copy the same mu vector along the time domain
+    if len(dipole_moments.shape) == 2:  # Time dependence is not supplied
+        dipole_moments = np.tile(dipole_moments,
+                                 (realizations, timesteps, 1, 1))
 
-    absorption_time_domain=0
+    absorption_td = 0
     if use_damping:
-        damp=np.exp(-np.arange(0,timesteps)*dt/lifetime)
+        damp = np.exp(-np.arange(0, timesteps) * dt / lifetime)
     else:
-        damp=1
-    for xyz in range(0,3):
-        for real in range(0,realizations):
-            for m in range (0,n_sites):
-                for n in range(0,n_sites):
+        damp = 1
 
-                    absorption_time_domain+=U[real,:,m,n]*mu[real,:,m,xyz]*mu[real,0,n,xyz]/realizations*damp
-    return absorption_time_domain
+    for xyz in range(3):
+        for real in range(realizations):
+            for m in range(n_sites):
+                for n in range(n_sites):
+                    absorption_td += (time_evolution_operator[real, :, m, n] *
+                                      dipole_moments[real, :, m, xyz] *
+                                      dipole_moments[real, 0, n, xyz] /
+                                      realizations * damp)
+    return absorption_td
 
-def absorb_time_to_freq(absorb_time,total_time,dt,pad=0,smoothdamp=True,smoothdamp_start_percent=10):
+
+def absorb_time_to_freq(absorb_time, config):
     """
     Convert time domain absorption to an absorption spectrum.
 
     Args:
         absorb_time (numpy.ndarray): Time domain absorption.
-        total_time (float): Total time duration of the absorption signal.
-        dt (float): Time step size.
-        pad (int, optional): Number of zero padding points for higher frequency resolution. Default is 0.
-        smoothdamp (bool, optional): Whether to smooth the transition to the padded region with an exponential damping. Default is True.
-        smoothdamp_start_percent (int, optional): Percentage of the time domain absorption affected by smoothing. Default is 10.
+        config (dict): Configuration dictionary containing parameters:
+            - total_time (float): Total time duration of the absorption signal.
+            - dt (float): Time step size.
+            - pad (int): Number of zero padding points for higher frequency 
+                resolution.
+            - smoothdamp (bool): Whether to smooth the transition to the padded 
+                region with an exponential damping.
+            - smoothdamp_start_percent (int): Percentage of the time domain 
+                absorption affected by smoothing.
 
     Returns:
         tuple: (numpy.ndarray, numpy.ndarray)
             - Absorption spectrum in the frequency domain.
             - Corresponding frequency axis.
-
-    Notes:
-        This function performs a Fast Fourier Transform (FFT) on the time domain absorption data to obtain the absorption spectrum
-        in the frequency domain. Optional smoothing can be applied to the transition to the padded region to reduce artifacts.
     """
-    absorb=np.pad(absorb_time,(0,pad)) #zero_padding for higher frequency resolution
-    
-    if smoothdamp:  #smooth the transition to padded region with an exponential damp, 
-                    #smoothdamp_start_percent % of the time domain absorbtion is affected.
-        absorb_steps=int(total_time//dt)-1
-        damp_start=int((100-smoothdamp_start_percent)/100* absorb_steps)
-        absorb=smooth_damp_to_zero(absorb,damp_start,absorb_steps)
-    #fft to frequency domain
-    absorb_f=np.fft.fftshift(np.fft.fft(absorb))
-    x_axis=-units.hbar*2*np.pi*np.fft.fftshift(np.fft.fftfreq(int((total_time+dt)/dt)+pad, d=dt*units.t_unit))
-    absorb_f=(absorb_f.real-absorb_f.real[0])/np.max(absorb_f.real-absorb_f.real[0])
-    return absorb_f,x_axis
+    total_time = config.get("total_time")
+    dt = config.get("dt", 1)
+    pad = config.get("pad", 0)
+    smoothdamp = config.get("smoothdamp", True)
+    smoothdamp_start_percent = config.get("smoothdamp_start_percent", 10)
+    # Zero padding for higher frequency resolution
+    absorb = np.pad(absorb_time, (0, pad))
 
+    if smoothdamp:
+        # Smooth the transition to the padded region with an exponential damp
+        absorb_steps = int(total_time // dt) - 1
+        damp_start = int((100 - smoothdamp_start_percent) / 100 * absorb_steps)
+        absorb = smooth_damp_to_zero(absorb, damp_start, absorb_steps)
+
+    # FFT to frequency domain
+    absorb_f = np.fft.fftshift(np.fft.fft(absorb))
+    freq = np.fft.fftfreq(int((total_time + dt) / dt) + pad,
+                          d=dt * units.t_unit)
+    x_axis = -units.hbar * 2 * np.pi * np.fft.fftshift(freq)
+    absorb_f_max = np.max(absorb_f.real - absorb_f.real[0])
+    absorb_f = (absorb_f.real - absorb_f.real[0]) / absorb_f_max
+    return absorb_f, x_axis

torchnise/nise.py
@@ -169,6 +169,7 @@ def t_correction(S,n_sites,realizations,device,E,Eold,T_correction,kBT,MLNISE_Mo
         dummy2[1-CD>0]=S_clone.clone()[1-CD>0]/norm[1-CD>0]
         dummy3=dummy1*dummy2
         S[1-CD>0,kk[1-CD>0],ii]=dummy3[1-CD>0]
+        return S
 
 def NISE_averaging(Hfull,realizations,psi0,total_time,dt,T,save_Interval=1,T_correction="None",averaging_method="standard",
          lifetime_factor=5,device="cpu", saveCoherence=False,saveU=False,MLNISE_inputs=None):
@@ -329,7 +330,14 @@ def run_NISE(H,realizations, total_time,dt, initialState,T, spectral_funcs,save_
     if mode.lower() =="absorption":
         avg_absorb_time = np.average(all_absorb_time, axis=0, weights=weights)
         pad=int(aborption_padding/(dt*torchnise.units.t_unit))
-        absorb_f, x_axis = absorb_time_to_freq(avg_absorb_time,total_time,dt,pad=pad)
+        absorb_config = {
+            "total_time":total_time,
+            "dt": dt,
+            "pad": pad,
+            "smoothdamp": True,
+            "smoothdamp_start_percent": 10
+        }
+        absorb_f, x_axis = absorb_time_to_freq(avg_absorb_time, absorb_config)
         return absorb_f, x_axis
     else:
         return avg_output, torch.linspace(0,total_time,avg_output.shape[0])

torchnise/sd_from_noise.py
@@ -234,7 +234,7 @@ def optimize_lambda(A, C, sparcity_penalty, l1_norm_penalty, solution_penalty, n
         lr (float, optional): Learning rate for the optimization algorithm. Defaults to 0.01.
         device (str, optional): Device for computation ('cuda' or 'cpu'). Defaults to 'cuda'.
         initial_guess (torch.Tensor, optional): Initial guess for the optimization. Defaults to None.
-        verbose (bool, optional): decide if infprmation should be printed
+        verbose (bool, optional): decide if information should be printed
 
     Returns:
         torch.Tensor: Optimized solution tensor.
@@ -296,42 +296,71 @@ def optimize_lambda(A, C, sparcity_penalty, l1_norm_penalty, solution_penalty, n
 
     return return_lambda
 
-def optimize_lambda_nnls(A, b,initial_guess=None, max_iter=1000,lr=0.01):
+def optimize_lambda_nnls(A, b,initial_guess=None, max_iter=1000,lr=0.01,verbose=False):
+    """
+    Perform non-negative least squares optimization using PyTorch.
+    
+    Args:
+        A (torch.Tensor): Matrix for the linear system.
+        b (torch.Tensor): Target vector.
+        initial_guess (torch.Tensor, optional): Initial guess for the optimization. Defaults to None.
+        max_iter (int, optional): Maximum number of iterations. Defaults to 1000.
+        lr (float, optional): Learning rate for the optimization algorithm. Defaults to 0.01.
+        erbose (bool, optional): decide if information should be printed
+    
+    Returns:
+        torch.Tensor: Optimized solution tensor.
+    """
     def A_transpose(y):
         return A.T @ y
+
     lambda_ij = A_transpose(b) if initial_guess is None else initial_guess
-    
-    lambda_ij=lambda_ij.detach().clone()
-    lambda_ij.requires_grad=True
-    return_lambda = lambda_ij 
-    optimizer = torch.optim.LBFGS([lambda_ij],lr=lr,line_search_fn="strong_wolfe") 
-    A @ torch.abs(lambda_ij)
+    lambda_ij = lambda_ij.detach().clone()
+    lambda_ij.requires_grad = True
+    return_lambda = lambda_ij
+
+    optimizer = torch.optim.LBFGS([lambda_ij], lr=lr, line_search_fn="strong_wolfe")
+    min_loss = torch.inf
+    no_improvement_iters = 0
+
     def closure():
         optimizer.zero_grad()
         loss = torch.norm(A @ torch.abs(lambda_ij) - b) ** 2
         loss.backward()
         return loss
-    min_loss=torch.inf
-    no_improvement_iters=0
+
     for i in range(max_iter):
-        
         optimizer.step(closure)
-        loss=closure().item()
+        loss = closure().item()
+
         if i % 10 == 0:
             print(f'Step {i}, Loss: {loss}')
-        if loss<min_loss:
-            min_loss=loss
-            no_improvement_iters=0
-            return_lambda=lambda_ij
+
+        if loss < min_loss:
+            min_loss = loss
+            no_improvement_iters = 0
+            return_lambda = lambda_ij
         else:
-            no_improvement_iters +=1
-        if no_improvement_iters >20:
-            print("no improvement for 20 iters")
+            no_improvement_iters += 1
+
+        if no_improvement_iters > 20:
+            print("No improvement for 20 iterations")
             break
+
     return torch.abs(return_lambda).detach()
     
 
 def adjust_tensor_length(a, l):
+    """
+    Adjust the length of a tensor by trimming or padding with zeros.
+
+    Args:
+        a (torch.Tensor): Input tensor.
+        l (int): Desired length of the output tensor.
+
+    Returns:
+        torch.Tensor: Adjusted tensor of length l.
+    """
     if len(a) == l:
         return a
     elif len(a) > l:
@@ -344,6 +373,17 @@ def adjust_tensor_length(a, l):
     return a
 
 def ensure_tensor_on_device(array, device='cuda',dtype=torch.float):
+    """
+    Ensure the input is a PyTorch tensor on the specified device. And move it if its not
+
+    Args:
+        array (np.ndarray or torch.Tensor): Input array or tensor.
+        device (str, optional): Desired device ('cuda' or 'cpu'). Defaults to 'cuda'.
+        dtype (torch.dtype, optional): Desired data type. Defaults to torch.float.
+
+    Returns:
+        torch.Tensor: Tensor on the specified device with the desired data type.
+    """
     if isinstance(array, np.ndarray):
         # Convert numpy array to PyTorch tensor and move to the specified device
         return torch.from_numpy(array).to(device).to(dtype)
@@ -355,7 +395,7 @@ def ensure_tensor_on_device(array, device='cuda',dtype=torch.float):
 
 def sd_reconstruct_superresolution(auto, dt, T, sparcity_penalty=1, l1_norm_penalty=1, 
                                    solution_penalty=10000,negative_penalty=1,ljnorm_penalty=0 ,j=0.5, lr=0.01, max_iter=1000, eta=1e-7, 
-                                   tol=1e-7, device='cuda', cutoff=None, frequencies=None,dampings=None,
+                                   tol=1e-7, device='cuda', cutoff=None, frequencies=None,linewidths=None,
                                    sample_frequencies=None, top_n=False,top_tresh=False, second_optimization=False,chunk_memory=1e9,auto_length_debias=None,auto_length_return=None):
     """
     Reconstruct the super-resolution spectral density from the autocorrelation function.
@@ -376,6 +416,8 @@ def sd_reconstruct_superresolution(auto, dt, T, sparcity_penalty=1, l1_norm_pena
         tol (float): Tolerance for convergence in the optimization.
         device (str): Device for computation ('cuda' or 'cpu').
         cutoff (float, optional): Cutoff for damping. Defaults to None.
+        frequencies (torch.tensor, optional): Frequencies for peaks that should be included in the optimization, otherwise default values are used
+        linewidths (torch.tensor, optional): Linewidths for peaks that should be included in the optimization, otherwise default values are used
         sample_frequencies (torch.Tensor, optional): Frequencies for sampling the spectral density. Defaults to None.
         top_n (int): If not False, only the top n coefficients are used. Defaults to False.
         top_tresh (float): Alternative to top_n chooses all coefficients above a treshold
@@ -403,13 +445,13 @@ def sd_reconstruct_superresolution(auto, dt, T, sparcity_penalty=1, l1_norm_pena
     t_axis_very_orig = dt *torch.arange(0, len(auto_very_orig), device=device)
     if frequencies==None:
         frequencies = torch.arange(0, 0.2, 0.00005, device=device) / units.hbar
-    if dampings==None:
-        dampings = torch.arange(1, 200, 0.5, device=device) / units.hbar
+    if linewidths==None:
+        linewidths= torch.arange(1, 200, 0.5, device=device) / units.hbar
 
-    damping_term = torch.exp(-dampings[None, :] * t_axis[:, None])
+    linewidths_term = torch.exp(-linewidths[None, :] * t_axis[:, None])
     frequency_term = torch.cos(frequencies[None, :] * t_axis[:, None])
 
-    A = damping_term[:, :, None] * frequency_term[:, None, :]
+    A = linewidths_term[:, :, None] * frequency_term[:, None, :]
 
     auto_orig = auto.clone()
     auto = auto[:N_cut]
@@ -418,9 +460,9 @@ def sd_reconstruct_superresolution(auto, dt, T, sparcity_penalty=1, l1_norm_pena
                                 negative_penalty, ljnorm_penalty, j, eta, max_iter=max_iter, 
                                 tol=tol, lr=lr, device=device)
     if top_n:
-        if top_n>len(dampings) * len(frequencies):
+        if top_n>len(linewidths) * len(frequencies):
             warnings.warn("top_n larger than total elements, using all available coefficients.")
-            top_n=len(dampings) * len(frequencies)
+            top_n=len(linewidths) * len(frequencies)
         _, indices = torch.topk(lambda_ij.flatten(), top_n) #removed abs since we don't want negative peaks anyway
         
         if second_optimization:         
@@ -429,7 +471,7 @@ def sd_reconstruct_superresolution(auto, dt, T, sparcity_penalty=1, l1_norm_pena
             for k in range(top_n):
                 i = original_indices[0][k]
                 j = original_indices[1][k]
-                A_new[:, k] = torch.exp(-dampings[i] * t_axis_full) * torch.cos(frequencies[j] * t_axis_full) 
+                A_new[:, k] = torch.exp(-linewidths[i] * t_axis_full) * torch.cos(frequencies[j] * t_axis_full) 
             
             lambda_ij_new = optimize_lambda_nnls(A_new,auto_orig,initial_guess=lambda_ij.flatten()[indices]) #torch.linalg.lstsq(A_new, auto_orig)[0] #
             lambda_ij_debias = torch.zeros_like(lambda_ij, device=device)
@@ -450,7 +492,7 @@ def sd_reconstruct_superresolution(auto, dt, T, sparcity_penalty=1, l1_norm_pena
             for k in range(top_n):
                 i = original_indices[0][k]
                 j = original_indices[1][k]
-                A_new[:, k] = torch.exp(-dampings[i] * t_axis_full) * torch.cos(frequencies[j] * t_axis_full) 
+                A_new[:, k] = torch.exp(-linewidths[i] * t_axis_full) * torch.cos(frequencies[j] * t_axis_full) 
             
             lambda_ij_new = optimize_lambda_nnls(A_new,auto_orig,initial_guess=lambda_ij.flatten()[indices]) #torch.linalg.lstsq(A_new, auto_orig)[0] #
             lambda_ij_debias = torch.zeros_like(lambda_ij, device=device)
@@ -473,15 +515,15 @@ def sd_reconstruct_superresolution(auto, dt, T, sparcity_penalty=1, l1_norm_pena
         J_new = torch.zeros_like(sample_frequencies)
        
         # Calculate the chunk size that would use less than the specified chunk_memory
-        chunk_size = int(chunk_memory / (3*len(frequencies) * len(dampings) * auto.element_size()))
+        chunk_size = int(chunk_memory / (3*len(frequencies) * len(linewidths) * auto.element_size()))
         chunk_size = max (chunk_size,1)
         for i in range(0, sample_frequencies.shape[0], chunk_size):
             max_idx = min(i + chunk_size, sample_frequencies.shape[0])
             sample_freq_chunk = sample_frequencies[i:max_idx]
-            T_frequencies_gamma = sample_freq_chunk[:, None, None] * dampings[None, :, None] * np.pi * 2
+            T_frequencies_gamma = sample_freq_chunk[:, None, None] * linewidths[None, :, None] * np.pi * 2
 
-            term1 = T_frequencies_gamma / (dampings[None, :, None]**2 + (sample_freq_chunk[:, None, None] - frequencies[None, None, :])**2)
-            term2 = T_frequencies_gamma / (dampings[None, :, None]**2 + (sample_freq_chunk[:, None, None] + frequencies[None, None, :])**2)
+            term1 = T_frequencies_gamma / (linewidths[None, :, None]**2 + (sample_freq_chunk[:, None, None] - frequencies[None, None, :])**2)
+            term2 = T_frequencies_gamma / (linewidths[None, :, None]**2 + (sample_freq_chunk[:, None, None] + frequencies[None, None, :])**2)
 
             J_new[i:max_idx] = (lambda_ij[None, :, :] * (term1 + term2)).sum(dim=[1, 2]) 
         J_new=J_new.detach().cpu().numpy()
@@ -489,9 +531,9 @@ def sd_reconstruct_superresolution(auto, dt, T, sparcity_penalty=1, l1_norm_pena
         auto_super = torch.zeros_like(auto_very_orig)
         for i in range(0, len(t_axis_full), chunk_size):
             max_idx = min(i + chunk_size, t_axis_full.shape[0])
-            damping_term_chunk = torch.exp(-dampings[None, :] * t_axis_full[i:max_idx, None])
+            linewidths_term_chunk = torch.exp(-linewidths[None, :] * t_axis_full[i:max_idx, None])
             frequency_term_chunk = torch.cos(frequencies[None, :] * t_axis_full[i:max_idx, None])
-            A_chunk = (damping_term_chunk[:, :, None] * frequency_term_chunk[:, None, :]).reshape(max_idx - i, len(dampings) * len(frequencies))
+            A_chunk = (linewidths_term_chunk[:, :, None] * frequency_term_chunk[:, None, :]).reshape(max_idx - i, len(linewidths) * len(frequencies))
             auto_super[i:max_idx] = A_chunk @ lambda_ij.flatten()
         auto_super = auto_super.detach().cpu().numpy()
 
@@ -502,14 +544,14 @@ def sd_reconstruct_superresolution(auto, dt, T, sparcity_penalty=1, l1_norm_pena
             for k in range(top_n):
                 i = original_indices[0][k]
                 j = original_indices[1][k]
-                A_new[:, k] = torch.exp(-dampings[i] * t_axis_very_orig) * torch.cos(frequencies[j] * t_axis_very_orig) 
+                A_new[:, k] = torch.exp(-linewidths[i] * t_axis_very_orig) * torch.cos(frequencies[j] * t_axis_very_orig) 
             auto_super_debias = A_new @ lambda_ij_new.flatten()
             for k in range(top_n):
                 i = original_indices[0][k]
                 j = original_indices[1][k]
-                T_frequencies_gamma= sample_frequencies*dampings[i] * np.pi * 2
-                term1 = T_frequencies_gamma / (dampings[i]**2 + (sample_frequencies - frequencies[j])**2)
-                term2 = T_frequencies_gamma / (dampings[i]**2 + (sample_frequencies + frequencies[j])**2)
+                T_frequencies_gamma= sample_frequencies*linewidths[i] * np.pi * 2
+                term1 = T_frequencies_gamma / (linewidths[i]**2 + (sample_frequencies - frequencies[j])**2)
+                term2 = T_frequencies_gamma / (linewidths[i]**2 + (sample_frequencies + frequencies[j])**2)
                 J_new_debias += lambda_ij_new[k]*(term1 + term2)
             J_new_debias=J_new_debias.detach().cpu().numpy()
             auto_super_debias=auto_super_debias.detach().cpu().numpy()