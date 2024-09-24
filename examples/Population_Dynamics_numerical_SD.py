fixed saveu, added different equilibrium option to lifetime calculation,

examples/Population_Dynamics_numerical_SD.py
@@ -6,13 +6,13 @@ from torchnise.spectral_functions import spectral_drude_lorentz_heom, spectral_n
 
 import torchnise
 
-for i in range(20):
+for chunk in range(200):
     energy_unit="cm-1"
     time_unit="fs"
     torchnise.units.set_units(e_unit=energy_unit,t_unit=time_unit)
     dt=1
     total_time=100000
-    realizations=100
+    realizations=8
     device="cpu" #"cuda" for GPU "cpu" for CPU
     if device=="cuda":
         torch.backends.cuda.preferred_linalg_library(backend="magma") #bottleneck for gpu calculations is torch.linalg.eigh which we found to be slightly faster with this backend
@@ -69,8 +69,8 @@ for i in range(20):
 
 
     population, xaxis_p = torchnise.nise.run_nise(H,realizations, total_time,dt, initialState,T, spectral_funcs,
-                t_correction=T_correction1,mode=Mode1,averaging_method=Averaging1, device=device,max_reps=5,save_interval=10,
-                save_u= True, save_u_file=f"examples/data/u_{i}.pt")
+                t_correction=T_correction1,mode=Mode1,averaging_method=Averaging1, device=device,max_reps=8,save_interval=100,
+                save_u= True, save_u_file=f"examples/data/u_{chunk}.pt")
 
     lh_complex_pop=[0]*len(lh_complexes)
     for i in range(n_sites):
@@ -82,6 +82,8 @@ for i in range(20):
     plt.ylabel("population")
     plt.xlim([torch.min(xaxis_p),torch.max(xaxis_p)])
     plt.ylim([0,1])
+    
     plt.legend()
-    plt.show()
+    plt.savefig(f"examples/data/fig{i}.pdf")
+    #plt.show()
     plt.close()

torchnise/nise.py
@@ -374,8 +374,6 @@ def run_nise(h, realizations, total_time, dt, initial_state, temperature,
     save_u = mode.lower() == "absorption" or save_u
     weights = []
     all_output = torch.zeros(num_chunks, save_steps, n_states)
-    if save_u:
-        all_u = torch.zeros((num_chunks, save_steps, n_states,n_states),dtype=torch.complex64)
 
     if mode.lower() == (
             "population" and t_correction.lower() in ["mlnise", "tnise"] and
@@ -408,7 +406,13 @@ def run_nise(h, realizations, total_time, dt, initial_state, temperature,
             absorb_time = absorption_time_domain(u, mu)
             all_absorb_time.append(absorb_time)
         if save_u:
-            all_u [i // chunk_size, :, :, :] = torch.mean(u, dim=0)
+            if save_u and save_u_file!=None:
+                if "." in save_u_file:
+                    name,ending=save_u_file.split(".")
+                else:
+                    name=save_u_file
+                    ending="pt"
+                torch.save(u,f"{name}_{i}.{ending}")
         all_output[i // chunk_size, :, :] = pop_avg
 
 
@@ -425,8 +429,7 @@ def run_nise(h, realizations, total_time, dt, initial_state, temperature,
     else:
         lifetimes = None
         avg_output = np.average(all_output, axis=0, weights=weights)
-    if save_u and save_u_file!=None:
-        torch.save(torch.mean(all_u,dim=0),save_u_file)
+    
 
     if mode.lower() == "absorption":
         avg_absorb_time = np.average(all_absorb_time, axis=0, weights=weights)