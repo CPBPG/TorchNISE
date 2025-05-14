ebc3e4b5a2ad3bd0191548f4d0b4ad529c71c8c6
Yannick Holtkamp <yannick.holtkamp@gmail.com>
2025-05-14 14:15:01 +0200

    Changed setup and Readme for version 0.3.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="torchnise",
-    version="0.3.0",
+    version="0.3.1",
     author="Yannick Holkamp, Emiliano Godinez, Ulrich Kleinekathöfer",
     author_email="yholtkamp@constructor.university",
     description="several nise extensions. ",
